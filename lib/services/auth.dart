ed546b4cb6776cd72afebcf573e956c60469a782
EYoung75 <eyoung75.ey@gmail.com>
2020-05-18 15:29:48 -0500

    implemented initial functionality for creating firebase user instance when using anonymous login

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,13 +1,19 @@
+import 'package:brew_crew/models/user.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  //create user object based off returned firebase user object
+  User _userfromFireBaseUser(FirebaseUser user) {
+    return user != null ? User(uid: user.uid) : null;
+  }
   
   Future anonSignIn() async {
     try {
       AuthResult result = await _auth.signInAnonymously();
       FirebaseUser user = result.user;
-      return user;
+      return _userfromFireBaseUser(user);
     } catch(err) {
       print(err.toString());
       return null;
