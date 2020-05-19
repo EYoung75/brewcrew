925c3ddabeb5d210a01c97be6450898aad6eb617
EYoung75 <eyoung75.ey@gmail.com>
2020-05-18 21:02:24 -0500

    Finished creating form for handling custom user settings

--- lib/screens/home/setttings_forms.dart
+++ lib/screens/home/setttings_forms.dart
@@ -25,7 +25,67 @@ class _SettingsFormState extends State<SettingsForm> {
             style: TextStyle(
               fontSize: 18,
             ),
-          )
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          TextFormField(
+            decoration: textInputDecoration.copyWith(hintText: "Name"),
+            validator: (value) {
+              value.isEmpty ? "Please enter a name" : null;
+            },
+            onChanged: (value) {
+              setState(() {
+                _currentName = value;
+              });
+            },
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          DropdownButtonFormField(
+              decoration: textInputDecoration,
+              value: _currentSugars ?? 0,
+              items: sugars.map((sugar) {
+                return DropdownMenuItem(
+                  child: Text("$sugar sugars"),
+                  value: sugar,
+                );
+              }).toList(),
+              onChanged: (value) {
+                setState(() {
+                  _currentSugars = value;
+                });
+              }),
+          SizedBox(height: 20),
+          Slider(
+            min: 100,
+            max: 900,
+            activeColor: Colors.brown[_currentStrength],
+            inactiveColor: Colors.brown[_currentStrength],
+            value: (_currentStrength ?? 100).toDouble(),
+            divisions: 8,
+            onChanged: (value) {
+              setState(() {
+                _currentStrength = value.round();
+              });
+            },
+          ),
+          SizedBox(height: 20),
+          RaisedButton(
+            onPressed: () async {
+              print(_currentName);
+              print(_currentSugars);
+              print(_currentStrength);
+            },
+            color: Colors.pink[400],
+            child: Text(
+              "Update",
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
         ],
       ),
     );
